b46d3ec08a57407e053e14dadcb2d3e3c72d7713
akaryrye <ralldrin@gmail.com>
2020-05-25 22:44:39 -0700

    add picture to title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import sallyMd from "../assets/images/sallysuction-md.jpg";
 
 export default function Title () {
 
@@ -6,9 +8,10 @@ export default function Title () {
       <div className='row'>
          <div className='col'>
             <div className="jumbotron jumbotron-fluid">
-               <div className="container">
-                  <h1 className="display-4">Sally's Fine Vintage Toys</h1>
-               </div>
+               <Row className="container">
+                  <h1 className="display-4 col-7 col-sm-8 col-lg-10 text-center">Sally's Fine Vintage Toys</h1>
+                  <img src={sallyMd} className="col-5 col-sm-4 col-lg-2"></img>
+               </Row>
             </div> 
          </div>
       </div>
